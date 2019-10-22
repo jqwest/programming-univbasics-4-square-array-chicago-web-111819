301a605daa9e71de60079896b37058bb9bbe5410
john L <johnleavell@gmail.com>
2019-10-22 02:07:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,6 @@
 def square_array(array)
-  new_array = []
-  array = [1, 2, 3]
-  array.each do |array|
-    new_array.push(array**2)
-  end
-  return new_array
+  squared = []
+    array.each { |element| squared << element ** 2 }
+    squared
   # your code here
 end
